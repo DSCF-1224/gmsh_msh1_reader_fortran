1f74925bd37715390e03828f5e95e6ae93b982fd
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-06-22 21:28:27 +0900

    Add the main `module` file

---
+++ src/gmsh_msh1_reader.f90
@@ -0,0 +1,3 @@
+!> Fortran module for reading [Gmsh](https://gmsh.info/doc/texinfo/gmsh.html) [MSH file format version 1](https://gmsh.info/doc/texinfo/gmsh.html#MSH-file-format-version-1-_0028Legacy_0029)
+module gmsh_msh1_reader
+end module gmsh_msh1_reader
